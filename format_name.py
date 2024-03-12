f787e1f1ca9d59fb9b10766e1c8f40755170a150
michal-fityka <81447683+michal-fityka@users.noreply.github.com>
2024-03-12 21:18:30 +0100

    Update format_name.py

--- format_name.py
+++ format_name.py
@@ -1,1 +1,7 @@
-
+def format_name(f_name,s_name):
+  formated_f_name = f_name.title()
+  formated_s_name = s_name.title()
+  return f"{formated_f_name} {formated_s_name}"
+  #return f_name.title() + " " + s_name.title()
+output = format_name("michal", "fityka")
+print(format_name("michal","fityka"))
