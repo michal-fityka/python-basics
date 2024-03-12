249ce976dff06c32d863696e461c22e4091b37f4
michal-fityka <81447683+michal-fityka@users.noreply.github.com>
2024-03-12 23:22:54 +0100

    Update format_name.py

--- format_name.py
+++ format_name.py
@@ -1,7 +1,9 @@
 def format_name(f_name,s_name):
+  if (f_name == "" or s_name == ""):
+    return f"Name or second name are empty"
   formated_f_name = f_name.title()
   formated_s_name = s_name.title()
   return f"{formated_f_name} {formated_s_name}"
   #return f_name.title() + " " + s_name.title()
 output = format_name("michal", "fityka")
-print(format_name("michal","fityka"))
+print(format_name(input("What is your first name?"),input("What is your second name?")))
